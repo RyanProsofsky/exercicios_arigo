c6e5064d7b6e41602ebbf81476f16bd78a0ddd00
Ryan ✅ <ry.prosofsky@gmail.com>
2024-08-02 16:32:44 -0300

    aletracao

--- exercicios_GitHub/Desafios-01.js
+++ exercicios_GitHub/Desafios-01.js
@@ -1,25 +1,31 @@
 // Declarar uma variável chamada `myvar`, sem valor.
+let myvar
 
 
 // Após declarada, atribua o valor 10 à variável `myvar`.
+myvar = 10
 
 
 // Declare uma nova variável chamada `soma`, e adicione uma instrução somando os valores 15 e 8.
+let soma = 15 + 8
 
 
 // Atribua à variável `soma` todo o valor dela, somando 1, usando o operador de soma abreviado.
-
+soma += 1
 
 // Atribua à variável `soma` todo o valor dela, multiplicando por 3, usando o operador de multiplicação abreviado.
-
+soma *= 3
 
 // Qual é o valor da variável `soma` até aqui?
-
+console.log(soma)
+console.log(72)
 
 // Declare uma variável chamada `souninja`, atribuindo à ela o valor booleano que representa `verdadeiro`.
+let souNinja = true
 
 
 // Declare uma variável chamada `comida` que recebe um array com os valores 'arroz', 'feijão' e 'ovo'.
+let comida = ['arroz','feijao','ovo']
 
 
 // Digite a instrução que imprime o valor de 'feijao', que está na variável `comida`.
