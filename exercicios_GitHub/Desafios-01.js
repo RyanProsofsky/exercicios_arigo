4c492c184b114c92e6330e0ef2510d567490685d
Ryan ✅ <ry.prosofsky@gmail.com>
2024-08-19 17:38:08 -0300

    mega sena

--- exercicios_GitHub/Desafios-01.js
+++ exercicios_GitHub/Desafios-01.js
@@ -29,15 +29,26 @@ let comida = ['arroz','feijao','ovo']
 
 
 // Digite a instrução que imprime o valor de 'feijao', que está na variável `comida`.
+console.log(comida[1])
 
 
 // Digite o código que verifica se a variável `soma' é igual a variável `myvar` (testando também o tipo).
-
-
+if(soma === myvar){
+    console.log("É igual")
+}else{
+    console.log("Não é igual")
+}
 // Digite o código que verifica se a variável `myvar` é menor ou igual à variável `soma`.
-
+if(myvar >= soma){
+    console.log("é maior ou igual")
+}else{
+    console.log("Não é maior nem igual")
+}
 
 // Crie uma função chamada `divisao` que receba como parâmetro dois números, e retorne o resultado da divisão entre eles.
-
+function divisao(n1,n2) {
+    return n1 / n2
+}
 
 // Invoque a função criada acima, passando os parâmetros 10 e 2.
+console.log(divisao(10,2))
---
+++ megaSena/megaSena.js
@@ -0,0 +1,31 @@
+//Sortear 6 numeros de 1 a 60 sem repetir.
+
+let numeros = []
+
+for (let index = 0; index < 6; index++) {
+    let gerador = Math.floor(Math.random() * (60 - 1 + 1)) + 1
+
+    if(gerador) {     
+        numeros[index] = gerador
+    }
+}
+
+console.log(numeros)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
