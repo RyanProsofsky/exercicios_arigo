16454aaa1a8df5952c7d3b8c6bed9778c6b42797
Ryan ✅ <ry.prosofsky@gmail.com>
2024-08-22 17:40:41 -0300

    exercicio

---
+++ sorteioJogadores/sorteio.js
@@ -0,0 +1,154 @@
+// Sortear Times
+// sortear 3 times, time para o Gilson, time para o joao e time para o Paulo.
+// Cada time deve ter apenas 1 jogador em cada posição e obviamente sem repetir jogadores dentre os 3 times.
+
+const joao = []
+const paulo = []
+const gilson = []
+let sorteio
+
+const GO = [
+    "Gianluigi Donnarumma",
+    "Lukas Hradecky",
+    "Yann Sommer",
+    "Mike Maignan",
+    "Manuel Neuer",
+    "Marc-Andre ter Stegen",
+    "Jan Oblak",
+    "Ederson",
+    "Emiliano Dibu Martinez",
+    "Alisson"
+];
+Math.floor(Math.random() * GO.length)
+
+const ZG = [
+    "Micky Van de Ven",
+    "Goncalo Inacio",
+    "Jules Koundé",
+    "Ben White",
+    "Mats Hummels",
+    "Jonathan Tah",
+    "Ibrahima Konaté",
+    "Pau Torres",
+    "Josko Gvardiol",
+    "Ruben Dias",
+    "Lucas Hernandez",
+    "Alphonso Davies",
+    "Matthijs de Ligt",
+    "Alessandro Bastoni",
+    "Ronald Araujo",
+    "John Stones",
+    "Marquinhos",
+    "Virgil Van Dijk",
+    "William Saliba",
+    "Antonio Rüdiger"
+];
+
+Math.floor(Math.random() * ZG.length)
+
+const LD = [
+    "Trent Alexander-Arnold",
+    "Jeremie Frimpong",
+    "Dani Carvajal",
+    "Achraf Hakimi",
+    "Kyle Walker"
+];
+Math.floor(Math.random() * LD.length)
+
+const LE = [
+    "Ferland Mendy",
+    "Theo Hernandez",
+    "Alejandro Grimaldo",
+    "Federico Dimarco",
+    "Andy Robertson"
+];
+Math.floor(Math.random() * LE.length)
+
+const MC = [
+    "James Maddison",
+    "Julian Brandt",
+    "Warren Zaire-Emery",
+    "Enzo Fernández",
+    "Federico Valverde",
+    "Lucas Paquetá",
+    "Granit Xhaka",
+    "João Palhinha",
+    "Bernardo Silva",
+    "Gavi",
+    "Hakan Calhanoglu",
+    "Eduardo Camavinga",
+    "Alexis Mac Allister",
+    "Bruno Guimarães",
+    "Xavi Simons",
+    "Joshua Kimmich",
+    "Frenkie de Jong",
+    "Nicolo Barella",
+    "Jamal Musiala",
+    "Kai Havertz",
+    "Kevin De Bruyne",
+    "Bruno Fernandes",
+    "Pedri",
+    "Aurelién Tchouaméni",
+    "Florian Wirtz",
+    "Declan Rice",
+    "Toni Kroos",
+    "Martin Ødegaard",
+    "Jude Bellingham",
+    "Rodri"
+];
+Math.floor(Math.random() * MC.length)
+
+const AT = [
+    "Viktor Gyökeres",
+    "Robert Lewandowski",
+    "Niclas Füllkrug",
+    "Paulo Dybala",
+    "Dusan Vlahovic",
+    "Victor Boniface",
+    "Alexander Isak",
+    "Ollie Watkins",
+    "Cristiano Ronaldo",
+    "Victor Osimhen",
+    "Lautaro Martínez",
+    "Cole Palmer",
+    "Son Heung-Min",
+    "Mohamed Salah",
+    "Erling Haaland",
+    "Harry Kane",
+    "Kylian Mbappé"
+];
+Math.floor(Math.random() * AT.length)
+
+const PD = [
+    "Jack Grealish",
+    "Leroy Sané",
+    "Khvicha Kvaratskhelia",
+    "Luis Díaz",
+    "Rodrygo",
+    "Bukayo Saka",
+    "Phil Foden"
+];
+Math.floor(Math.random() * PD.length)
+
+const PE = [
+    "Christian Pulisic",
+    "Ousmane Dembélé",
+    "Rafael Leão",
+    "Lionel Messi",
+    "Vinícius Júnior",
+    "Antoine Griezmann"
+];
+Math.floor(Math.random() * PE.length)
+
+
+
+console.log(GO[sorteio])
+
+
+
+
+
+
+
+
+
